e6f77799f91f0e905c076d741e6db7d1269fdfbb
Fabuss254 <26258237+fabuss254@users.noreply.github.com>
2022-02-06 10:40:15 +0100

    Modified some LOVE config for performance gain

--- conf.lua
+++ conf.lua
@@ -1,3 +1,10 @@
 function love.conf(t)
 	t.console = true
+
+	t.modules.joystick = false
+	t.modules.mouse = false
+	t.modules.video = false
+	t.modules.touch = false
+	t.modules.physic = false
+	t.modules.thread = false
 end
